df540e4029c500b162e224f2f0974be031a9a7a4
Krish Jha <krishjha1121@gmail.com>
2025-08-04 23:00:33 +0545

    Time: 252 ms (13.16%), Space: 29.3 MB (8.14%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.size();
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            map<char, int> mpp;  
+
+            for (int j = i; j < n; j++) {
+                mpp[s[j]]++; 
+
+                int mini = INT_MAX;
+                int maxi = INT_MIN;
+
+                for (auto it : mpp) {
+                    mini = min(mini, it.second);
+                    maxi = max(maxi, it.second);
+                }
+
+                res += (maxi - mini);  
+            }
+        }
+
+        return res;
+    }
+};
