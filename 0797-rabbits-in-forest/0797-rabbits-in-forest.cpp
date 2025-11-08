3837b6575e0744093e865d229a978beb3a1d7cec
Krish Jha <krishjha1121@gmail.com>
2025-11-08 10:20:35 +0530

    Time: 0 ms (100%), Space: 12.3 MB (38.81%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& nums, int total = 0) {
+        unordered_map <int,int> mpp;
+        for (int it : nums) {
+            mpp[it]++;
+        }
+        for (auto& p : mpp){
+            total += ceil((double)p.second / (p.first + 1)) * (p.first + 1);
+        }
+        return total;
+    }
+};
