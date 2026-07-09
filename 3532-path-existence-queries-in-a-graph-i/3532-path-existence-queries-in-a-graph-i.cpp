9ea117b1c7c47a512814560a0e260033f79d79ec
Krish Jha <krishjha1121@gmail.com>
2026-07-09 15:37:33 +0545

    Time: 70 ms (21.64%), Space: 252 MB (11.23%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,17 +1,52 @@
+class DSU {
+    vector<int> parent, size;
+public:
+    DSU(int n) {
+        parent.resize(n);
+        size.assign(n, 1);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y) return;
+
+        if (size[x] < size[y])
+            swap(x, y);
+
+        parent[y] = x;
+        size[x] += size[y];
+    }
+};
+
 class Solution {
 public:
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<int> component(n, 0);
-        int compNo = 0;
-        for(int i = 1; i < n; i++){
-            if(nums[i] - nums[i - 1] > maxDiff){
-                compNo++;
+    vector<bool> pathExistenceQueries(int n,vector<int>& nums,int maxDiff,
+    vector<vector<int>>& queries) {
+        vector<pair<int, int>> arr;
+        for (int i = 0; i < n; i++){
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());
+        DSU dsu(n);
+        for (int i = 1; i < n; i++) {
+            if (arr[i].first - arr[i - 1].first <= maxDiff) {
+                dsu.unite(arr[i].second, arr[i - 1].second);
             }
-            component[i] = compNo;
         }
         vector<bool> ans;
-        for(auto &it : queries){
-            ans.push_back(component[it[0]] == component[it[1]]);
+        for (auto &q : queries) {
+            ans.push_back(dsu.find(q[0]) == dsu.find(q[1]));
         }
         return ans;
     }
