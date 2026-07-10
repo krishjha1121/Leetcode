f9ae7831095f1d3e3e00c60982ea95c2005d7678
Krish Jha <krishjha1121@gmail.com>
2026-07-10 18:11:43 +0545

    Time: 1 ms (26.68%), Space: 9.5 MB (5.1%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:    
+    vector<vector<int>> dp;
+    int solve(int i, int j){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = solve(i - 1, j);
+        int left = solve(i, j - 1);
+
+        return dp[i][j] = up + left;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        dp.assign(m, vector<int>(n, -1));
+        return solve(m - 1, n - 1);
+    }
+};
