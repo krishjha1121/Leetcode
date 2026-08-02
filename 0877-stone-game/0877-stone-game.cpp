e508b3ee5382a4d9b70875995e43d7691b05df1c
Krish Jha <krishjha1121@gmail.com>
2026-08-02 11:33:14 +0530

    Time: 20 ms (9%), Space: 19.8 MB (7.37%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    int n;
+    vector<vector<int>> dp;
+    int solve(int i, int j, vector<int>& piles){
+        if(i == j){
+            return piles[i];
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        int left = piles[i] - solve(i + 1, j, piles);
+        int right = piles[j] - solve(i , j - 1, piles);
+        return dp[i][j] = max(left, right);
+    }
+public:
+    bool stoneGame(vector<int>& piles) {
+        n = piles.size();
+        dp.assign(n, vector<int>(n, -1));
+        return solve(0, n - 1, piles) > 0;
+    }
+};
