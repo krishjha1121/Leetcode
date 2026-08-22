57c70874dd9a9c47a2d8ffdadddab370fcfa3700
Krish Jha <krishjha1121@gmail.com>
2026-08-22 11:51:54 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.77%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,19 +14,15 @@ public:
     bool isBalanced(TreeNode* root) {
         return dfs(root) != -1;
     }
-
 private:
     int dfs(TreeNode* node){
         if(node == NULL){
             return 0;
         }
-
         int lh = dfs(node -> left);
         if(lh == -1) return -1;
-
         int rh = dfs(node -> right);
         if(rh == -1) return -1;
-
         if(abs(lh - rh) > 1) return -1;
         return 1 + max(lh , rh);
     }
