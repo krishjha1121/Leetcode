5efa09a1cfcfad55d3686349180afe52971f8b41
Krish Jha <krishjha1121@gmail.com>
2026-09-13 11:12:27 +0530

    Time: 33 ms (84.75%), Space: 15.8 MB (37.67%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+    int largestOverlap(vector<vector<int>>& img1,vector<vector<int>>& img2) {
         int n = img1.size();
+        vector<pair<int, int>> ones1, ones2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (img1[i][j] == 1) ones1.push_back({i, j});
+                if (img2[i][j] == 1) ones2.push_back({i, j});
+            }
+        }
+
+        unordered_map<int, int> freq;
         int ans = 0;
-        for (int dr = -(n - 1); dr < n; dr++) {
-            for (int dc = -(n - 1); dc < n; dc++) {
-                int overlap = 0;
+        for (auto [r1, c1] : ones1) {
+            for (auto [r2, c2] : ones2) {
+                int dr = r2 - r1;
+                int dc = c2 - c1;
 
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        int ni = i + dr;
-                        int nj = j + dc;
-
-                        if (ni >= 0 && ni < n &&
-                            nj >= 0 && nj < n &&
-                            img1[i][j] == 1 &&
-                            img2[ni][nj] == 1) {
-                            overlap++;
-                        }
-                    }
-                }
-                ans = max(ans, overlap);
+                int key = dr * 100 + dc;
+                freq[key]++;
+                ans = max(ans, freq[key]);
             }
         }
 
